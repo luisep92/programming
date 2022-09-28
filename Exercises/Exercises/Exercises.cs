3a460e49ecb9bdfe4ca474228eee08dac8dfb7a7
luisep92 <luis@escolano.es>
2022-09-28 16:45:54 +0200

    Ejercicios 28/9

---
+++ Exercises/Exercises/Exercises.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Exercises
+    {
+        public static bool isEven(int n)
+        {
+            return n % 2 == 0;
+        }
+
+        public static bool isPrime(int n)
+        {
+            int max = (int)Math.Sqrt(n) + 1;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static double GetAreaCircle(double r)
+        {
+            if (r < 0)
+                return double.NaN;
+            return Math.PI * (r * r);
+        }
+        public static double GetAreaRectangle(double b, double h)
+        {
+            if (b <= 0 || h <= 0)
+                return double.NaN;
+            return b * h;
+        }
+      //  public static double VectorDistance()
+    }
+}
---
+++ Exercises/Exercises/Vector3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    internal class Vector3
+    {
+        double x;
+        double y;
+        double z;
+
+        public Vector3(double a, double b, double c)
+        {
+            x = a;
+            y = b;
+            z = c;
+        }
+    }
+}
