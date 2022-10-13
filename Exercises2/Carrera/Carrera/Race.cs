634d5dfab0df0bc77b7ef4764c0513d5bd0cfe46
luisep92 <luis@escolano.es>
2022-10-13 19:13:40 +0200

    a

---
+++ Exercises2/Carrera/Carrera/Race.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrera
+{
+    internal class Race
+    {
+        public static List<Racer> generateRacers(int n)
+        {
+            List<Racer> list = new List<Racer> ();
+            
+           // n Racer racer1
+
+
+            return list;
+        }
+
+
+
+        public static int RandomNegativeOrPositive()
+        {
+            Random r = new Random();
+
+            if (r.Next(0, 1) == 0)
+                return -1;
+            return 1;
+        }
+    }
+}
---
+++ Exercises2/Carrera/Carrera/Racer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrera
+{
+    internal class Racer
+    {
+        public string name;
+        public double position;
+
+        public void Move(int quantity)
+        {
+            Random r = new Random();
+            position += quantity + r.Next(0,3) * Race.RandomNegativeOrPositive();
+        }
+
+
+        
+    }
+}
