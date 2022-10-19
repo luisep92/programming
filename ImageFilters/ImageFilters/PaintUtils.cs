aa62ddc4cb05c0c838f817a92d8cc287374ab352
luisep92 <luis@escolano.es>
2022-10-19 17:12:08 +0200

    image

---
+++ ImageFilters/ImageFilters/PaintUtils.cs
@@ -0,0 +1,23 @@
+﻿using DAM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFilters
+{
+    internal class PaintUtils
+    {
+        public static void FillRectangle(Image image, RGBA color, int width, int height, int x, int y)
+        {
+            for (int i = x; i < x + width; i++)
+            {
+                for (int j = y; j < y + height; j++)
+                {
+                    image.SetPixel(i, j, color);
+                }
+            }
+        }
+    }
+}
---
+++ ImageFilters/ImageFilters/Program.cs
@@ -0,0 +1,15 @@
+﻿using DAM;
+namespace ImageFilters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Luis\\Desktop\\Images\\test.png";
+            Image img = new Image();
+            img.Config(800, 600, new RGBA(1, 0.864, 0.793, 1));
+            Image.FillRectangle(img, new RGBA(0, 1, 0, 1), 100, 100, 100, 100);
+            img.Save(path);
+        }
+    }
+}
