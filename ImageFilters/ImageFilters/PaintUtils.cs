1c5dd6bdad782b9e1d452bb38b84a05fed2e927d
luisep92 <luis@escolano.es>
2022-10-23 12:50:05 +0200

    paintutils

--- ImageFilters/ImageFilters/PaintUtils.cs
+++ ImageFilters/ImageFilters/PaintUtils.cs
@@ -19,5 +19,69 @@ namespace ImageFilters
                 }
             }
         }
+
+        public static void GreyscaleFilter(Image image)
+        {
+            for(int i = 0; i < image.Width; i++)
+            {
+                for(int j = 0; j < image.Height; j++)
+                {
+                    RGBA color = ColorToGrayScale(image.GetPixelAt(i,j));
+                    image.SetPixel(i, j, color);
+                }
+            }
+        }
+        
+        public static void MultiplyColorsFilter(Image img, RGBA inputColor)
+        {
+            for(int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    RGBA outputColor = MultiplyColors(img.GetPixelAt(i,j), inputColor);
+
+                    img.SetPixel(i, j, outputColor);
+                }
+            }
+        }
+
+        public static Image FlipImageFilter(Image inputImg)
+        {
+            Image outputImg = new Image();
+            outputImg.Config(inputImg.Width, inputImg.Height);
+
+            for (int i = 0; i < outputImg.Width; i++)
+            {
+                for(int j = 0; j < outputImg.Height; j++)
+                {
+                    RGBA currentPixel = inputImg.GetPixelAt(i,j);
+                    outputImg.SetPixel(i, outputImg.Height - j, currentPixel);
+                }
+            }
+            return outputImg;
+        }
+
+        public static RGBA MultiplyColors(RGBA c1, RGBA c2)
+        {
+            RGBA output = new RGBA();
+            output.r = c1.r * c2.r;
+            output.g = c1.b * c2.g;
+            output.b = c1.b * c2.b;
+            output.a = c1.a * c2.a;
+
+            return output;
+        }
+
+        public static RGBA ColorToGrayScale(RGBA color)
+        {
+            double tone = GetAverage(color.r, color.b, color.g);
+
+            return new RGBA(tone, tone, tone, 1);
+        }
+
+        public static double GetAverage(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
     }
 }
--- ImageFilters/ImageFilters/Program.cs
+++ ImageFilters/ImageFilters/Program.cs
@@ -1,15 +1,49 @@
 ﻿using DAM;
+
 namespace ImageFilters
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Test3();
+        }
+        static void Test2()
+        {
+            string path = "C:\\Users\\Luis\\Desktop\\Images\\";
+
+            Image img = new Image();
+            img.Load(path +"pepe.jpg");
+            PaintUtils.GreyscaleFilter(img);
+            img.Save(path + "pepe2.jpg");
+        }
+        static void Test1()
+        {
             string path = "C:\\Users\\Luis\\Desktop\\Images\\test.png";
             Image img = new Image();
             img.Config(800, 600, new RGBA(1, 0.864, 0.793, 1));
             PaintUtils.FillRectangle(img, new RGBA(0, 1, 0, 1), 200, 300, 100, 100);
             img.Save(path);
+        }
+        static void Test3()
+        {
+            string path = "C:\\Users\\Luis\\Desktop\\Images\\";
+
+            Image img = new Image();
+            RGBA color = new RGBA(1, 0, 0, 1);
+            img.Load(path + "pepe.jpg");
+            
+            PaintUtils.MultiplyColorsFilter(img, color);
+            img.Save(path + "pepe3.jpg");
+        }
+        static void Test4()
+        {
+            string path = "C:\\Users\\Luis\\Desktop\\Images\\";
+
+            Image img = new Image();
+            img.Load(path + "pepe.jpg");
+            img = PaintUtils.FlipImageFilter(img);
+            img.Save(path + "pepe4.jpg");
         }
     }
 }
