cac9e90638790741553e9ec09eb8002111a66ab8
luisep92 <luis@escolano.es>
2023-03-26 17:30:31 +0200

    usings

--- Ev3/Buscaminas/BuscaminasLib/Board1.cs
+++ Ev3/Buscaminas/BuscaminasLib/Board1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuscaminasLib
+﻿namespace BuscaminasLib
 {
     /// <summary>
     /// Board with a bidimensional array.
