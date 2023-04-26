aabf745e21413ce877844c94338730b02952c999
luisep92 <luis@escolano.es>
2023-04-26 17:01:43 +0200

    cvbnm,

---
+++ Research/API/API/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public struct Persona
+    {
+    }
+
+    public static class Utils
+    {
+        public static void HacerAlgo<T>(this T p) where T : struct
+        {
+            Console.WriteLine("Hago algo");
+        }
+
+        
+    }
+}
